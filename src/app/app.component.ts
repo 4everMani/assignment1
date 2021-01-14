bffc08201093cd16d73c94b781845f567bab6c38
4everMani <jaiswal.manish49@gmail.com>
2021-01-14 19:52:28 +0530

    Assignment1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user.model';
+import { UserServices } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[UserServices]
+  
 })
-export class AppComponent {
-  title = 'Assignment1';
+export class AppComponent implements OnInit {
+  users : User[]=[]
+  userNumber : number
+  constructor(private userService : UserServices){};
+
+  ngOnInit(){
+    console.log(`hello $(this.userService.users)`)
+    
+    this.users = this.userService.users
+    console.log(this.userService.users.values);
+  }
+
+  
+
+  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserServices } from './user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [UserServices],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
